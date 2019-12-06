f5492a9a44c37504c9d8f909e0fce9c66cee5466
Your Name <you@example.com>
2019-12-07 00:24:10 +0300

    folders

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const cors = require('cors');
 const app = express();
 app.use(cors());
@@ -11,8 +12,19 @@ const port = 2222;
 
 app.post('/send-client-info', (req, res) => {
     let params = req.body;
-    console.log('params:', params);
-    res.json({msg: 'success'});
+    let filename = __dirname + '/logs/logs.json';
+    fs.readFile(filename, 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            let logList = JSON.parse(data); //now it an object
+            logList.push(params);
+            logList = JSON.stringify(logList); //convert it back to json
+            fs.writeFile(filename, logList, 'utf8', () => {
+                res.json({msg: 'success'});
+            }); // write it back
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
