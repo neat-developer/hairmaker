443500f46791a7605391e5019162e2bd8cce7e55
Your Name <you@example.com>
2019-12-07 00:43:58 +0300

    update

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
@@ -8,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
 const port = 2222;
 
 app.post('/send-client-info', (req, res) => {
@@ -21,10 +23,41 @@ app.post('/send-client-info', (req, res) => {
             logList.push(params);
             logList = JSON.stringify(logList); //convert it back to json
             fs.writeFile(filename, logList, 'utf8', () => {
+
+
+                fs.readFile(__dirname + '/config/mail.json', 'utf8', function readFileCallback(err, user) { // Почта
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        user = JSON.parse(user); //now it an object
+                        let transport = nodemailer.createTransport({
+                            service: "Yandex",
+                            auth: {
+                                user: user.name,  // to be replaced by actual username and password
+                                pass: user.password
+                            }
+                        });
+
+                        transport.sendMail({
+                            from: 'beauty@hairmaker.club',
+                            to: ['beauty@hairmaker.club', 'webangulardeveloper@gmail.com'],
+                            subject: 'Регистрация на сайте "hairmaker"',
+                            text: 'Пользователь оставил свой номер',
+                            html: '<!DOCTYPE html><html lang="ru"><head> <meta charset="UTF-8"> <title>Пользователь хочет записаться</title>' +
+                                '</head><body>' +
+                                '<h1>Пользователь оставил свой номер</h1>' +
+                                '<p>Имя: ' +params.name + '</p>' +
+                                '<p>Номер телефона: ' + params.phone + '</p>' +
+                                '</body></html>'
+                        })
+                    }
+                });
+
                 res.json({msg: 'success'});
             }); // write it back
         }
     });
 });
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
